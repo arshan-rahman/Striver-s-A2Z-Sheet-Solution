a5a525f745fe285e54b4ff7718f5195f0b3fc047
Arshan Rahman <90138239+arshan-rahman@users.noreply.github.com>
2026-02-02 21:15:12 +0530

    Add files via upload

---
+++ Reverse Word.cpp
@@ -0,0 +1,33 @@
+#include <iostream> //for I/O
+#include <sstream> //for coverting string into word
+#include <vector> //for storing word in sperate dynamic format
+using namespace std; //c++ library
+
+string reverseWords(string s){
+    stringstream ss(s); //convert string into steam for easy word extarction
+    string word; //stores extracted word
+    vector<string> words; //store all words
+
+    while(ss>>word){ //read all words seprated by spaces
+        words.push_back(word); //adding word at the end
+    }
+
+    string result; //stores the final reverse word
+    for(int i = words.size()-1; i>=0;--i){ //starts at last word until index 0
+        result += words[i]; //stores the word in reverse order
+        if(i!=0){
+            result += " "; //add space after each word except for thr last word
+        }
+    }
+    return result;
+
+}
+
+int main(){
+    string s;
+    getline(cin,s); // reads the entire line
+
+    cout<<reverseWords(s);
+    return 0;
+
+}
